19fba8c6e639e8fa591d6b4fcd2801ccb6c3e1f3
Liam Warbox <liam.winterton.dev@gmail.com>
2022-11-10 09:51:18 +0000

    Container has support for two max widths

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import * as styles from './Header.module.scss'
 const Header = () => {
 	return (
 		<header className={styles.header}>
-			<Container>
+			<Container type="wide">
 				<p>This is the header</p>
 			</Container>
 		</header>
--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -4,10 +4,21 @@ import * as styles from './Container.module.scss'
 
 interface Props {
 	children: React.ReactNode
+	type: 'normal' | 'wide'
 }
 
 const Container = (props: Props) => {
-	return <div className={styles.container}>{props.children}</div>
+	const containerStyles = [styles.container]
+
+	if (props.type === 'wide') {
+		containerStyles.push(styles.wide)
+	}
+
+	return <div className={containerStyles.join(' ')}>{props.children}</div>
+}
+
+Container.defaultProps = {
+	type: 'normal',
 }
 
 export default Container
